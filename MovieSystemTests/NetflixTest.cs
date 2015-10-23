c6e1ef95043e0526d1a32ca9ae9ad05feb65c2c1
Kalle <Kalle.hallert@gmail.com>
2015-10-23 11:20:33 +0200

    First commit

---
+++ MovieSystemTests/NetflixTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieSystemTests
+{
+    [TestClass]
+    public class NetflixTest
+    {
+        [TestMethod]
+        public void Test_That_Facade_Can_Be_Created()
+        {
+            MovieFacade facade = new MovieFacade();
+            Assert.IsNotNull(facade);
+        }
+
+        [TestMethod]
+        public void Test_That_Netflix_Can_Be_Played()
+        {
+            MovieFacade facade = new MovieFacade();
+            facade.Play("Netflix");
+            Assert.IsTrue(facade.HasBeenPlayed);
+            //Assert.Fail();
+        }
+    }
+}
---
+++ MovieSystemTests/SFTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MovieSystemTests
+{
+    [TestClass]
+    public class SFTest
+    {
+        [TestMethod]
+        public void Test_That_Facade_Can_Be_Created()
+        {
+            MovieFacade facade = new MovieFacade();
+            Assert.IsNotNull(facade);
+        }
+
+        [TestMethod]
+        public void Test_That_SF_Can_Be_Played()
+        {
+            MovieFacade facade = new MovieFacade();
+            facade.Play("SF");
+            Assert.IsTrue(facade.HasBeenPlayed);
+            //Assert.Fail();
+        }
+    }
+}
